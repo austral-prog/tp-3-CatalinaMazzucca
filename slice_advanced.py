4a4a4e48d65da5ef7bd996eb0903c5f3ec027dc6
CatalinaMazzucca <162723477+CatalinaMazzucca@users.noreply.github.com>
2024-04-07 15:37:43 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,10 @@
 def slice_advanced():
-    # Código a implementar utilizando input.
+    frase = input("Ingrese su frase: ")
 
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+print(frase)
+print(frase[4:14:2])
+
+secuencia = input("Ingrese su secuencia: ")
+      
+print(secuencia)
+print(secuencia[4:11:2])
